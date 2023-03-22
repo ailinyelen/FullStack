2c869fb92d1a8cba6c3039f259f183609417ef62
Ailin Braccelarghe <a.braccelarghe@mercapsoftware.com>
2023-03-22 16:20:37 -0300

    up to exercise 3.14

--- index.js
+++ index.js
@@ -2,82 +2,98 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('common'))
 
-let contacts = [{ 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// Connection with DB
+const password = 'mongoDBtest'
 
-app.get('/', (request, response) => {
-    console.log('Get')
-    response.send('<h1>Notes</h1>')
-    })
-    
+const url = `mongodb+srv://ailinyelenb:${password}@test.vu51asg.mongodb.net/contactsApp?retryWrites=true&w=majority`
 
-app.get('/info', (request, response) => {
-    console.log('Get Info')
-    response.send(`<div><p>Phonebook has info for ${contacts.length} people</p><p> ${Date()} </p></div>`)
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+// Contact Schema
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: String
 })
+
+
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+
+// Get all contacts
 
 app.get('/api/persons', (request, response) => {
     console.log('Get Contacts')
-    response.json(contacts)
+    Contact.find({}).then(result => response.json(result))
 })
 
+// Get contact by ID
+
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const contact = contacts.find(each => each.id === Number(id))
-    console.log(contact)
-    contact 
-    ? response.json(contact) 
-    : response.status(404).json({error:'Contact Not Found'})
+    const contact =
+        Contact
+            .findById(request.params.id)
+            .then(contact => {
+                contact
+                    ? response.json(contact)
+                    : response.status(404).json({ error: 'Contact Not Found' })
+            })
+            .catch(error => next(error))
 })
+
+// Add new contact
 
 app.post('/api/persons', (request, response) => {
     console.log(request.body)
     const name = request.body.name
     const number = request.body.number
 
-    if (!name || !number) 
-        response.status(400).json({error:'Missing Parameters'})
+    if (!name || !number)
+        response.status(400).json({ error: 'Missing Parameters' })
+    /*
     else if (contacts.find(each => each.name === name))
         response.status(400).json({error:'Name already exists in phonebook'})
+    */
     else {
-        const id = contacts.length + 1
-        const contact = {name, number, id}
-        contacts = contacts.concat(contact)
+        const contact = new Contact({ name, number })
+        contact.save().then(result => {
+            console.log(`added ${name} number ${number} to phonebook`)
+        })
         response.json(contact)
     }
 })
 
+// Delete contact
+
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    contacts = contacts.filter(each => each.id !== Number(id))
-    console.log('deleted ', id)
-    response.status(204).end()
+    console.log('delete: ', request.params.id)
+    //console.log(Contact.findById(request.params.id))
+    Contact
+        .findByIdAndDelete(request.params.id)
+        .then(result => {
+            console.log('result: ', result)
+            response.status(204).end()
+        })
 })
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
