ce511652c64f34bbe5d37ef3b2436b5f7fa2139f
ailinyelen <33434576+ailinyelen@users.noreply.github.com>
2023-03-29 16:00:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/persons', contactsRouter)
+app.use('/api/contacts', contactsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
